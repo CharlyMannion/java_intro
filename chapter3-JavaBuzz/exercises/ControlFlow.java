95982b46aeb1b2cfe8df3f8b7e53d36162142651
Charly Mannion <charlottemannion100@gmail.com>
2018-06-04 16:13:14 +0100

    while loop

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,10 +1,22 @@
 // what imports will you need?
-
+import java.util.Scanner;
 // can you remember how to set up your main function?
-
+public class ControlFlow {
+  public static void main(String args[]) {
+    int a;
 // find a way to read in user input
-
-// write a method that will check if a number is odd or even
-// (assume user only ever enters integers)
-
-// print the answer to the console
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter an integer");
+    while ((a = in.nextInt()) != 17) {
+    // a = in.nextInt();
+    if (a % 2 == 0) {
+      System.out.println("You entered an even integer " + a);
+      System.out.println("Enter an integer");
+    }
+     else {
+       System.out.println("You entered an odd integer: " + a);
+       System.out.println("Enter an integer");
+     }
+   }
+  }
+}
