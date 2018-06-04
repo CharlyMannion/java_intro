fd519286b02e66cd716655743804bc7780b93325
Charly Mannion <charlottemannion100@gmail.com>
2018-06-04 13:31:11 +0100

    chapter 2 - string interpolation

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String x = "Charly";
     // declare the second name
-
+    String y = "Habin";
     // create your message
-
+    String newString = String.format("%s and %s are learning Java!", x, y);
     // print your message!
-
+    System.out.println(newString);
   }
 }
