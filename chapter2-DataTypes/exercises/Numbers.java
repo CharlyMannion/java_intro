ca127bfcc7c7b72a78c7f11538852e6432a38611
Charly Mannion <charlottemannion100@gmail.com>
2018-06-04 13:50:00 +0100

    numbers

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,12 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-
+    int bills = 200;
     // declare a double variable with a value
-
+    double rent = 866.66;
     // store the result of adding the two together in a variable
-
+    double total = bills + rent;
     // print it
-
+    System.out.println(total);
   }
 }
