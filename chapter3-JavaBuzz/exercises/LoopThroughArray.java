3a6c443a7fa92d52f8dba2c7c0a7ff5d9b459726
Charly Mannion <charlottemannion100@gmail.com>
2018-06-04 16:16:11 +0100

    loop through array

--- chapter3-JavaBuzz/exercises/LoopThroughArray.java
+++ chapter3-JavaBuzz/exercises/LoopThroughArray.java
@@ -1,3 +1,16 @@
 // can you remember how to set up your main function?
 
 // loop through an integer array and print out each element
+import java.util.ArrayList;
+import java.util.Arrays;
+// can you remember how to set up your main function?
+public class LoopThroughArray {
+  public static void main(String args[]) {
+// declare an array of Strings and print it
+    String[] stringArray = { "Charly", "Igor", "Ellie", "Habin", "Anders", "Aseel" };
+    System.out.println(stringArray[1]);
+    for (String name : stringArray) {
+      System.out.println(name);
+    }
+  }
+}
